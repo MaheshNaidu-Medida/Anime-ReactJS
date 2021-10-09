1c8c58f067da58612edaba46394cdcaf086921ce
Mahesh <mnaidu40@gmail.com>
2021-10-09 11:30:00 +0000

    Protected Route props spreading correction

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -2,13 +2,13 @@ import {Redirect, Route} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 const ProtectedRoute = props => {
+  const {path, component} = props
   const token = Cookies.get('ani_token')
-  if (token === null || undefined) {
+  if (token === null || token === undefined) {
     return <Redirect to="/login" />
   }
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Route {...props} />
+  return <Route exact path={path} component={component} />
 }
 
 export default ProtectedRoute
